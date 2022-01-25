6a0f12da1587afa30acd0bcd0463ddff36f3531d
MattyCi <mjciaravino@gmail.com>
2022-01-24 19:30:28 -0500

    Using proper response type for authentication error response messages

--- src/main/java/com/seasongg/user/controllers/AuthenticationController.java
+++ src/main/java/com/seasongg/user/controllers/AuthenticationController.java
@@ -1,8 +1,8 @@
 package com.seasongg.user.controllers;
 
 import com.seasongg.common.CommonController;
+import com.seasongg.common.RestResponse;
 import com.seasongg.common.SggService;
-import com.seasongg.season.models.SeasonResponse;
 import com.seasongg.user.models.AuthenticationRequest;
 import com.seasongg.user.models.RegistrationRequest;
 import com.seasongg.user.services.UserLoginService;
@@ -37,7 +37,7 @@ public class AuthenticationController extends CommonController {
         } catch (Exception e) {
             LOG.error("Unexpected error when user tried logging in with username: {}.",
                     authenticationRequest.getUsername(), e);
-            return new ResponseEntity<>(new SeasonResponse(SggService.UNKNOWN_ERROR,
+            return new ResponseEntity<>(new RestResponse(SggService.UNKNOWN_ERROR,
                     SggService.UNKNOWN_ERROR_TEXT), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
@@ -49,12 +49,12 @@ public class AuthenticationController extends CommonController {
         try {
             return ResponseEntity.ok(userRegisterService.registerUser(registrationRequest));
         } catch (SggService.SggServiceException e) {
-            return new ResponseEntity<>(new SeasonResponse(SggService.APPLICATION_ERROR, e.getMessage()),
+            return new ResponseEntity<>(new RestResponse(SggService.APPLICATION_ERROR, e.getMessage()),
                     HttpStatus.BAD_REQUEST);
         } catch (Exception e) {
             LOG.error("Unexpected error when user tried creating account with username: {}.",
                     registrationRequest.getUsername(), e);
-            return new ResponseEntity<>(new SeasonResponse(SggService.UNKNOWN_ERROR,
+            return new ResponseEntity<>(new RestResponse(SggService.UNKNOWN_ERROR,
                     SggService.UNKNOWN_ERROR_TEXT), HttpStatus.INTERNAL_SERVER_ERROR);
         }
 
