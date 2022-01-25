90b227f7a29993d8f8c5d6f22e39859566b3f609
MattyCi <mjciaravino@gmail.com>
2022-01-24 21:51:24 -0500

    Disabling default @SpringBootTest since we are only concerned with strict unit testing for now.

--- src/test/java/com/seasongg/SeasonggApplicationTests.java
+++ src/test/java/com/seasongg/SeasonggApplicationTests.java
@@ -1,9 +1,11 @@
 package com.seasongg;
 
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@Disabled("Disabling until integration tests are desired.")
 class SeasonggApplicationTests {
 
 	@Test
