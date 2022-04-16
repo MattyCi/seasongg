1882f16e9c46f8dbd8f0520b211dcd86e3440572
MattyCi <mjciaravino@gmail.com>
2022-04-16 15:29:24 -0400

    removing unused imports

--- src/main/java/com/seasongg/filters/auth/JwtRequestFilter.java
+++ src/main/java/com/seasongg/filters/auth/JwtRequestFilter.java
@@ -3,11 +3,7 @@ package com.seasongg.filters.auth;
 import com.seasongg.config.security.util.JwtUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
